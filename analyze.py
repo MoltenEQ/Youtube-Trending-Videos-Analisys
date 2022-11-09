3ec0b9793e77350f246da5490e364711cf22f423
MoltenEQ <molteneq.official@gmail.com>
2022-11-09 20:47:20 +0100

    Did some more processing

--- analyze.py
+++ analyze.py
@@ -6,7 +6,9 @@ pd.set_option('display.float_format', lambda x: '%.2f' % x) # Set numeric value 
 pd.options.plotting.backend = "plotly" # Use plotly to generate visualizations
 
 
-REGIONS = ["CA","DE","FR","GB","IN","JP","KR","RU","MX","US"] # TODO fix Russia
+# REGIONS = ["CA","DE","FR","GB","IN","JP","KR","RU","MX","US"] # TODO fix Russia
+
+REGIONS = ["CA"] # Canada only
 
 videos = []
 categories = []
@@ -16,6 +18,7 @@ for region_code in REGIONS:
     categories.append(pd.read_json(f"data/{region_code}_category_id.json"))
     print("Read files for region: " + region_code)
 
+## Leírás generálás
 # for i in range(10):
 #     print("### " + REGIONS[i] + " ###")
 #     # print(videos[i].info())
@@ -23,10 +26,37 @@ for region_code in REGIONS:
 
 ca_vid = videos[0]
 
+ca_vid_tag_count = []
 
-for col in ca_vid.columns:
-    print(f"{col}: {ca_vid[col].isna().sum()} - {ca_vid[col].isna().sum()/ca_vid[col].count()}")
+# Na értékek aránya
+for row in range(len(ca_vid.index)): # Number of rows
+    raw_tags = ca_vid['tags'][row]
+    tags_split = raw_tags.split('|')
+    for i in range(len(tags_split)):
+        tags_split[i] = tags_split[i].strip('\"') # idézőjelek törlése
+    if len(tags_split) == 1: 
+        if not tags_split[0] or tags_split[0] == "[none]": # ha üres string akkor hamissal tér vissza
+            tags_split = []
+    ca_vid_tag_count.append(len(tags_split))
 
+# Beillesztés
+
+ca_vid.insert(len(ca_vid.columns),"tag_count",ca_vid_tag_count)
+
+boxplot = ca_vid[["tag_count"]].boxplot()
+boxplot.show()
+
+
+# raw_tags = ca_vid['tags'][1]
+# tags_split = raw_tags.split('|')
+# for i in range(len(tags_split)):
+#     tags_split[i] = tags_split[i].strip('\"') # idézőjelek törlése
+# print(f"{1}: {len(tags_split)}")
+# print(tags_split)
+
+
+
+## Boxplotokhoz
 # boxplot = ca_vid[["views"]].boxplot()
 # boxplot.show()
 # boxplot = ca_vid[["likes"]].boxplot()
@@ -37,6 +67,7 @@ for col in ca_vid.columns:
 # boxplot.show()
 # print("plot")
 
+## Egyedi értékek aránya
 # print(ca_vid.describe())
 # for col in ca_vid:
 #     print(ca_vid[col].nunique())
