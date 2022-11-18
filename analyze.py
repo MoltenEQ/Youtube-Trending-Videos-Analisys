ff052c3e35c44c95b159086c23abb809ce05b76c
MoltenEQ <molteneq.official@gmail.com>
2022-11-18 20:05:01 +0100

    Finished data loading

--- analyze.py
+++ analyze.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import json
 import plotly
 import io
+import math
 
 pd.set_option('display.float_format', lambda x: '%.2f' % x) # Set numeric value print format
 pd.options.plotting.backend = "plotly" # Use plotly to generate visualizations
@@ -24,26 +26,44 @@ pd.options.plotting.backend = "plotly" # Use plotly to generate visualizations
 # ]
 REGIONS = ["CA","DE","FR","GB","IN","JP","KR","RU","MX","US"] # TODO fix Korea if we can
 
-# REGIONS = ["KR"] # Korea only
+# REGIONS = ["CA"] # Korea only
 
 video_lists = []
+raw_categories = []
 categories = []
+joined_data = []
 
 for region_code in REGIONS:
     # videos.append(pd.read_csv(f"data/{region_code}videos.csv",encoding="utf-16-be",on_bad_lines='skip'))
     video_lists.append(pd.read_csv(f"data/{region_code}videos.csv",on_bad_lines='skip'))
-    categories.append(pd.read_json(f"data/{region_code}_category_id.json"))
+    with open(f"data/{region_code}_category_id.json") as data_file:    
+        data = json.load(data_file) 
+    row_category_normalized = pd.json_normalize(data, ["items"], )
+    raw_categories.append(row_category_normalized)
     print("Read files for region: " + region_code)
 
+# Make categories usuable
+for i in range(len(REGIONS)):
+    print("Make categories usable for: " + REGIONS[i])
+    raw_category = raw_categories[i]
+    ids = raw_category["id"]
+    titles = raw_category["snippet.title"]
+    dict = {"id":ids,"category_title":titles}
+    category = pd.DataFrame(dict)
+    category['id'] = category['id'].astype(str)
+    categories.append(category)
+
+# Fix bad data
 for i in range(len(REGIONS)):
     print("Fixing bad data for region: " + REGIONS[i])
     video_list = video_lists[i]
+
     #Force column dtypes
     video_list['video_id'] = video_list['video_id'].astype(str)
     video_list['trending_date'] = video_list['trending_date'].astype(str)
     video_list['title'] = video_list['title'].astype(str)
     video_list['channel_title'] = video_list['channel_title'].astype(str)
-    video_list['category_id'] = video_list['category_id'].astype(int)
+    video_list['category_id'] = video_list['category_id'].astype(str)
     video_list['publish_time'] = video_list['publish_time'].astype(str)
     video_list['tags'] = video_list['tags'].astype(str)
     #We had errors with RU views
@@ -60,16 +80,20 @@ for i in range(len(REGIONS)):
     video_list['video_error_or_removed'] = video_list['video_id'].astype(bool)
     video_list['description'] = video_list['video_id'].astype(str)
 
+#Tags are bad, let's fix that
 for i in range(len(REGIONS)):
-    print("Info for region: " + REGIONS[i])
+    print("Fixing tags: " + REGIONS[i])
     video_list = video_lists[i]
-    #Tags are bad, let's fix that
     fixed_tags = []
     for row in range(len(video_list.index)):
         row_raw_tags = video_list['tags'][1]
         row_tags_split = row_raw_tags.split('|')
         for i in range(len(row_tags_split)):
             row_tags_split[i] = row_tags_split[i].strip('\"') # replace quotation marks
+        # Tags may have "[None]" in them
+        if len(row_tags_split) == 1: 
+            if not row_tags_split[0] or row_tags_split[0] == "[none]": # empy lists are falsy
+                row_tags_split = []
         fixed_tags.append(row_tags_split)
 
     fixed_tags_tupple = {'tags': fixed_tags}
@@ -82,38 +106,53 @@ for i in range(len(REGIONS)):
     # Fix publish time (from string to date)
     video_list['publish_time'] = pd.to_datetime(video_list['publish_time'])
 
-    # Describe the list
-    print("===============")
-    print(video_list.info())
-    print(video_list.describe())
+# Insert more data
+for i in range(len(REGIONS)):
+    print("Insert more data into: " + REGIONS[i])
+
+    #Insert count of tags
+    video_list = video_lists[i]
+    tag_count = []
+    for row in video_list['tags']:
+        tag_count.append(len(row))
+    video_list.insert(len(video_list.columns),"tag_count",tag_count)
+
+    # Insert length of description. NOTE: I've had to replace descriptions for the RU region
+    description_lengths = []
+    video_list['description'] = video_list['description'].fillna('') # We don't want NaN values, as they are considered float
+    for row in video_list['description']:
+        description_lengths.append(len(row))
+    video_list.insert(len(video_list.columns),"description_length",description_lengths)
+
+    # Add region
+    region = [REGIONS[i]] * len(video_list.index)
+    video_list.insert(len(video_list.columns),"region",region)
+
+# Join categories and videos
+for i in range(len(REGIONS)):
+    video_list = video_lists[i]
+    category = categories[i]
+    joined = video_list.join(category.set_index("id"), on="category_id")
+    joined_data.append(joined)
+
+# Merge all of them into one BIG set
+full_data = pd.concat(joined_data)
+
+# Describe the data
+for i in range(len(REGIONS)):
+    print(f"**====================================**")
+    print(f"**====== REGION : {REGIONS[i]} =======**")
+    print(f"**====================================**")
+    data = joined_data[i]
+    print(data.info())
+    print(data.describe())
 
 
-
-
-
-# ca_vid = videos[0]
-
-# ca_vid_tag_count = []
-
-# # Na értékek aránya
-# for row in range(len(ca_vid.index)): # Number of rows
-#     raw_tags = ca_vid['tags'][row]
-#     tags_split = raw_tags.split('|')
-#     for i in range(len(tags_split)):
-#         tags_split[i] = tags_split[i].strip('\"') # idézőjelek törlése
-#     if len(tags_split) == 1: 
-#         if not tags_split[0] or tags_split[0] == "[none]": # ha üres string akkor hamissal tér vissza
-#             tags_split = []
-#     ca_vid_tag_count.append(len(tags_split))
-
-# # Beillesztés
-
-# ca_vid.insert(len(ca_vid.columns),"tag_count",ca_vid_tag_count)
-
-
-
-
-
+print(f"**====================================**")
+print(f"**================ TOTAL =============**")
+print(f"**====================================**")
+print(full_data.info())
+print(full_data.describe())
 
 
 ## Boxplotokhoz
@@ -126,8 +165,3 @@ for i in range(len(REGIONS)):
 # boxplot = ca_vid[["comment_count"]].boxplot()
 # boxplot.show()
 # print("plot")
-
-## Egyedi értékek aránya
-# print(ca_vid.describe())
-# for col in ca_vid:
-#     print(ca_vid[col].nunique())
